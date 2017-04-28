0c080d1155399d6999e8b5c06e8c663bb66dbbf2
Dmitriy Onishchenko <donish@uw.edu>
2017-04-28 15:33:16 -0700

    changed login  activity

--- app/src/main/java/uw/tacoma/edu/paidaid/view/LoginActivity.java
+++ app/src/main/java/uw/tacoma/edu/paidaid/view/LoginActivity.java
@@ -1,11 +1,7 @@
 package uw.tacoma.edu.paidaid.view;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 import uw.tacoma.edu.paidaid.R;
 
@@ -15,17 +11,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
