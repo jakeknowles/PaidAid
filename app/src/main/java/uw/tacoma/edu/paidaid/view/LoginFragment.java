574f73df4d239fa23029f63122e73a2b9d3eede7
dmitriy <d.onish@gmai.com>
2017-05-08 16:40:44 -0700

    User able to login in now

--- app/src/main/java/uw/tacoma/edu/paidaid/view/LoginFragment.java
+++ app/src/main/java/uw/tacoma/edu/paidaid/view/LoginFragment.java
@@ -1,52 +1,65 @@
+/**
+ * Dmitriy Onishchenko
+ * Mobile Apps
+ *
+ * This is a login fragment for our app consisting of a login and sign up button.
+ *
+ */
+
 package uw.tacoma.edu.paidaid.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 import uw.tacoma.edu.paidaid.R;
 
 /** Login Fragment */
-public class LoginFragment extends Fragment implements View.OnClickListener{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private final static String LOGIN_URL
+            = "http://paidaid.x10host.com/login.php?";
+
+    private LoginActivity mLoginActivity;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+
+
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
     /** Login Fragment */
-    public static LoginFragment newInstance(String param1, String param2) {
+    public static LoginFragment newInstance() {
         LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
 
     }
 
@@ -56,6 +69,10 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        mEmailEditText = (EditText) view.findViewById(R.id.email_text);
+        mPasswordEditText = (EditText) view.findViewById(R.id.password_text);
+
+        // attach on click listener
         Button login = (Button) view.findViewById(R.id.login_button);
         login.setOnClickListener(this);
 
@@ -65,8 +82,51 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
         return view;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if(context instanceof LoginActivity) {
+            mLoginActivity = (LoginActivity) context;
+        }
+    }
 
 
+    /**
+     * Gets the text entered into the username and password fields and builds
+     * login url to be used with the AsyncTask
+     * @param v the view
+     * @return url to be used to with AsyncTask
+     */
+    private String buildLoginURL(View v) {
+
+        StringBuilder sb = new StringBuilder(LOGIN_URL);
+
+        try {
+
+            String username = mEmailEditText.getText().toString();
+            sb.append("email=");
+            sb.append(username);
+
+
+            String password = mPasswordEditText.getText().toString();
+            sb.append("&password=");
+            sb.append(URLEncoder.encode(password, "UTF-8"));
+
+            Log.i("LoginFragment", sb.toString());
+
+        }
+        catch(Exception e) {
+            Toast.makeText(v.getContext(), "Something wrong with the url" + e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * Helper method to launch new fragment if the user want to sign up.
+     */
     private void launchSignUpScreen() {
 
         getFragmentManager()
@@ -77,12 +137,29 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
 
     }
 
+
+    /**
+     * Helper method to create url and try logining in
+     */
+    private void tryLogin(View view) {
+
+        String url = buildLoginURL(view);
+
+        LoginTask task = new LoginTask();
+        task.execute(url);
+
+    }
+
+    /**
+     * Onclick method part of the onClick listener.
+     * @param v the view
+     */
     @Override
     public void onClick(View v) {
 
         switch(v.getId()){
             case R.id.login_button:
-                launchSignUpScreen();
+                tryLogin(v);
 
                 break;
             case R.id.sign_up_now_button:
@@ -90,6 +167,81 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
 
             default:
                 break;
+        }
+    }
+
+
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to Login Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+
+                if (status.equals("success")) {
+
+                    // go to home screen ///////////////////////////////////////////////
+                    Intent intent = new Intent(getActivity(), HomeActivity.class);
+                    startActivity(intent);
+
+
+                    Toast.makeText(mLoginActivity.getApplicationContext(), "Successfully Logged in!"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(mLoginActivity.getApplicationContext(), "Failed to Login: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(mLoginActivity.getApplicationContext(), "Something wrong with the Login " +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
         }
     }
 
