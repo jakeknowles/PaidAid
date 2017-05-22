39aa25d0edf2e135baf289d0d4d18f14656f9ab2
jakeknowles <jakepknowles@gmail.com>
2017-05-22 10:42:17 -0700

    Added XML layout for pick_up_request, fragment_request, and edited add_request

--- app/src/main/java/uw/tacoma/edu/paidaid/coreFeatures/AddRequestFragment.java
+++ app/src/main/java/uw/tacoma/edu/paidaid/coreFeatures/AddRequestFragment.java
@@ -41,7 +41,7 @@ public class AddRequestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_request_button, container, false);
+        return inflater.inflate(R.layout.fragment_add_request, container, false);
     }
 
 
--- app/src/main/java/uw/tacoma/edu/paidaid/coreFeatures/MyMessagesFragment.java
+++ app/src/main/java/uw/tacoma/edu/paidaid/coreFeatures/MyMessagesFragment.java
@@ -42,7 +42,7 @@ public class MyMessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_messages_button, container, false);
+        return inflater.inflate(R.layout.fragment_my_messages, container, false);
     }
 
 
