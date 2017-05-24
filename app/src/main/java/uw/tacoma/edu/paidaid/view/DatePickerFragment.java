0beb8e410cf70d8b34b8019754c0527657ce08f3
jakeknowles <jakepknowles@gmail.com>
2017-05-24 11:57:47 -0700

    new style for date picker

--- app/src/main/java/uw/tacoma/edu/paidaid/view/DatePickerFragment.java
+++ app/src/main/java/uw/tacoma/edu/paidaid/view/DatePickerFragment.java
@@ -16,6 +16,7 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
     }
 
 
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current date as the default date in the picker
@@ -24,8 +25,13 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month, day);
+        DatePickerDialog dpd = new DatePickerDialog(getActivity(),
+                android.support.design.R.style.Theme_Design,this,year,month,day);
+
+//        // Create a new instance of DatePickerDialog and return it
+//        return new DatePickerDialog(getActivity(), this, year, month, day);
+
+        return dpd;
 
     }
 
