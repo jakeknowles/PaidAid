90823cf6f478317cf6875c73b4b535c259fcce0e
jakeknowles <jakepknowles@gmail.com>
2017-05-28 23:08:25 -0700

    Finished most of testing, changed messages screen to "in progress"

--- app/src/test/java/uw/tacoma/edu/paidaid/RequestTest.java
+++ app/src/test/java/uw/tacoma/edu/paidaid/RequestTest.java
@@ -5,14 +5,19 @@ import org.junit.Test;
 import uw.tacoma.edu.paidaid.model.Request;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by jake on 5/22/17.
  */
 
 public class RequestTest {
-
-    public String requestJSON = "";
+    public String testJSON = "{\"user_id\":\"123456789\",\"username\":" +
+                             "\"test123\",\"email\":\"jake@gmail.com\",\"tip\":\"10.00\"," +
+                             "\"distance\":\"20\"," + "\"storename\":\"Costco\"," +
+                             "\"items_and_comments\":\", 2 Vacuums, , Box of Fruit Snacks, Almonds, 2 Tubs of Red Licorice, " +
+                             "Black 5 person Tent, 3 Sleeping Bags, 10 Cases of AAA Batteries,\"rating\":\"4.1\" }";
     public int theUserID = 1;
     public String theUsername = "test123";
     public String theEmail = "test@gmail.com";
@@ -22,17 +27,13 @@ public class RequestTest {
     public String theItemsAndComments = "7 boxes fruit snacks";
     public double theStarRating = 4.5;
 
-
     public Request mRequest = new Request(theUserID, theUsername, theEmail, theTip, theDistanceAway,
-            theStoreName, theItemsAndComments, theStarRating);
+                theStoreName, theItemsAndComments, theStarRating);
 
 
     @Test
-    public void parseRequestsJSON() {
-
-//        ArrayList<Request> mRequestList = Request.parseRequestsJSON(requestJSON);
-//        Request testRequest = mRequestList.get(0);
-
+    public void testRequestConstructor() {
+        assertNotNull(mRequest);
     }
 
     @Test
@@ -72,7 +73,22 @@ public class RequestTest {
 
     @Test
     public void testgetmStarRating() {
-        assertEquals(theStarRating, mRequest.getmStarRating());
+        assertTrue(theStarRating == mRequest.getmStarRating());
     }
 
+    @Test
+    public void parseRequestsJSON() {
+
+//        ArrayList<Request> mRequestList = Request.parseRequestsJSON();
+//        Request testRequest = mRequestList.get(0);
+//        assertEquals("User ID does not match", 1 == testRequest.getmUserID());
+//        assertTrue("Username does not match", "jake" == testRequest.getmUsername();
+//        assertTrue("Email does not match", "jake@gmail.com" == testRequest.getmEmail());
+//        assertTrue("Tip amount does not match", 5.0 == testRequest.getmTipAmount());
+//        assertEquals("Distace does not match", 5.0 == testRequest.getmDistanceAway());
+//        assertEquals("Store name does not match", "Costco" == testRequest.getmStoreName());
+//        assertEquals("Items does not match", "10 Boxes of AAA Batteries" == testRequest.getmItemsAndComments());
+//        assertEquals("Star Rating does not match", 4.2 == testRequest.getmStarRating());
+
+    }
 }
